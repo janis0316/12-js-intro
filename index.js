95b537ca985c2758081a27ebe6d67ba847bb2f5c
Laura <januleviciute.l@gmail.com>
2022-10-18 09:30:09 +0300

    Inital commint

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log(123);
+console.log('Labas rytas');
+console.log('2+2 =', 2+2);
+
+//Single line comment
+
+/*
+Multi line coment
+*/
+
+/*
+Esmines JS temos:
+- kintamieji - informacijos isiminimas
+- salygos sakiniai - palygininmas/ patikrinimas
+- ciklai- pasikartojanciu proceduru kartojimas iki kol reikia
+- funkcijos/klases - pasikartojancios logikos surinkimas i viena vieta
+- event listeners - (front'o) stebejjimas kada ir koki veiksma padare vartotojas
+*/
